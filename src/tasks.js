8f6c3e878f7eb2743183822469b90d3d137f8ce0
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-02-12 04:20:56 -0600

    move add task dom to different module

--- src/tasks.js
+++ src/tasks.js
@@ -14,4 +14,52 @@ export default class Tasks {
     static returnAllTask() {
         return Tasks.allTask
     }
+
+    static returnTaskDOM(task, index) { 
+        let itemList = document.createElement("li")
+        let itemInput = document.createElement("input")
+        let itemLabel = document.createElement("label")
+        let itemSpanTitle = document.createElement("span")
+        let itemSpanDue = document.createElement("span")
+        let itemSpanPriority = document.createElement("span")
+        let itemIcons = document.createElement("div")
+        let itemDelteBtn = document.createElement("button")
+        let itemEditBtn = document.createElement("button")
+        let itemDescriptionDiv = document.createElement("div")
+        let itemDescription = document.createElement("span")
+    
+        itemList.classList.add("task-item")
+        itemInput.type = "checkbox"
+        itemInput.id = `toggle${index}`
+        itemInput.classList.add("task-input")
+        itemLabel.setAttribute("for", `toggle${index}`)
+        itemLabel.classList.add("task-visisble")
+        itemSpanTitle.classList.add("task-title")
+        itemSpanDue.classList.add("task-due")
+        itemSpanPriority.classList.add("task-priority")
+        itemIcons.classList.add("icons")
+        itemDelteBtn.dataset.taskBtn = "delete"
+        itemEditBtn.dataset.taskBtn = "edit"
+        itemDescriptionDiv.classList.add("task-expand")
+    
+        itemSpanTitle.textContent = task.title
+        itemSpanDue.textContent = task.due
+        itemSpanPriority.textContent = task.priority
+        itemDescription.textContent = task.description
+        itemDelteBtn.textContent = "Delete"
+        itemEditBtn.textContent = "Edit"
+    
+        itemIcons.appendChild(itemDelteBtn)
+        itemIcons.appendChild(itemEditBtn)
+        itemLabel.appendChild(itemSpanTitle)
+        itemLabel.appendChild(itemSpanDue)
+        itemLabel.appendChild(itemSpanPriority)
+        itemLabel.appendChild(itemIcons)
+        itemDescriptionDiv.appendChild(itemDescription)
+        itemList.appendChild(itemInput)
+        itemList.appendChild(itemLabel)
+        itemList.appendChild(itemDescriptionDiv)
+    
+        return itemList
+    }
 }
